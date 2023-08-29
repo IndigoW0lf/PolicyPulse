810f34ec098f5977e71db623d457a9eb918808c6
Kai Indigo Wolf <kai.indigo.wolf@gmail.com>
2023-08-29 18:42:36 -0500

    Delete paths and forms for no longer needed 'add/edit/delete' legislation as app is taking a different path

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
+from flask import render_template
 from policyapp import app
+
+@app.route('/')
+def homepage():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
