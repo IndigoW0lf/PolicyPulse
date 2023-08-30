674822815ebc17bf7bc40e66224442c1dd495bc6
Kai Indigo Wolf <kai.indigo.wolf@gmail.com>
2023-08-30 13:46:47 -0500

    Add new routes for Cosponsor, Relatedbill, and Politician data

--- policyapp/routes.py
+++ policyapp/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, request, redirect, url_for
 from policyapp import app, db
 from policyapp.models import *
     
+#Legislation Routes
 @app.route('/legislations')
 def get_legislations():
   legislations = Legislation.query.all()
@@ -30,3 +31,48 @@ def filter():
     else:
         legislations = Legislation.query.all()
     return render_template('filter_results.html', legislations=legislations)
+
+# Politician Routes
+@app.route('/politicians')
+def get_politicians():
+    politicians = Politician.query.all()
+    return render_template('politicians.html', politicians=politicians)
+
+@app.route('/politician/<int:id>', methods=['GET'])
+def politician_detail(id):
+    politician = Politician.query.get(id)
+    if not politician:
+        return "Politician not found", 404
+    return render_template('politician_detail.html', politician=politician)
+
+# CoSponsor Routes
+@app.route('/legislation/<int:id>/co-sponsors')
+def get_cosponsors(id):
+    legislation = Legislation.query.get(id)
+    if not legislation:
+        return "Legislation not found", 404
+    co_sponsors = legislation.co_sponsors
+    return render_template('cosponsors.html', co_sponsors=co_sponsors)
+
+@app.route('/co-sponsor/<int:id>')
+def cosponsor_detail(id):
+    cosponsor = CoSponsor.query.get(id)
+    if not cosponsor:
+        return "Co-sponsor not found", 404
+    return render_template('cosponsor_detail.html', cosponsor=cosponsor)
+
+# RelatedBill Routes
+@app.route('/legislation/<int:id>/related-bills')
+def get_related_bills(id):
+    legislation = Legislation.query.get(id)
+    if not legislation:
+        return "Legislation not found", 404
+    related_bills = legislation.related_bills
+    return render_template('related_bills.html', related_bills=related_bills)
+
+@app.route('/related-bill/<int:id>')
+def related_bill_detail(id):
+    related_bill = RelatedBill.query.get(id)
+    if not related_bill:
+        return "Related bill not found", 404
+    return render_template('related_bill_detail.html', related_bill=related_bill)
