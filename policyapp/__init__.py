17334dfb9a1fb46056b5a97f36459e70ffd85ed8
Kai Indigo Wolf <kai.indigo.wolf@gmail.com>
2023-08-31 19:33:33 -0500

    Move DB URI to env variables for security

--- policyapp/__init__.py
+++ policyapp/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:7777@localhost:5432/policypulse'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', 'default_value_if_not_set')  #
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-# Import routes after initializing app and db
 import policyapp.routes
